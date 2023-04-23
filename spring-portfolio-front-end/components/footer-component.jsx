7a5ba2b10c4c835767079d24f75dc2be786f522a
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-04-22 17:48:58 -0700

    Use fixed-bottom class helper

--- spring-portfolio-front-end/components/footer-component.jsx
+++ spring-portfolio-front-end/components/footer-component.jsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Home.module.css';
 export default function Footer({ footerText }) {
   return (
     <div>
-      <footer className="bg-dark" style={{textAlign: "center", position: "fixed", width: "100%", bottom: "0"}}>
+      <footer className="fixed-bottom text-center bg-dark" >
         <span className='text'>{footerText}</span>
       </footer>
     </div>
