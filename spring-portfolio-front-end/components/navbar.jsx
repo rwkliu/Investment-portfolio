a0a4dfe543ef46f3b89b3e386420070681c43b1a
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-04-24 21:48:22 -0700

    change property names
    Change class to className
    change crossorigin to crossOrigin

--- spring-portfolio-front-end/components/navbar.jsx
+++ spring-portfolio-front-end/components/navbar.jsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function Navbar({ brandName, notificationName, numAlerts }) {
   return (
-    <nav class="navbar bg-dark"> 
-      <div class="container-fluid">
-        <Link href="/" class="navbar-brand mb-0" style={{fontSize: "35px", color: "white"}}>{brandName}</Link>
-        <button type="button" class="btn btn-primary">
+    <nav className="navbar bg-dark"> 
+      <div className="container-fluid">
+        <Link href="/" className="navbar-brand mb-0" style={{fontSize: "35px", color: "white"}}>{brandName}</Link>
+        <button type="button" className="btn btn-primary">
           {notificationName}<span class="badge text-bg-secondary">{numAlerts}</span>
         </button>
       </div>
