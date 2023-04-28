c4d591adc5c6defe40d84e7e222ea1646f6b86bc
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-04-27 20:44:43 -0700

    Initialize backend

---
+++ MySpringPortfolioBackend/src/main/java/com/liur/myspringportfoliobackend/MySpringPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.liur.myspringportfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringPortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ MySpringPortfolioBackend/src/test/java/com/liur/myspringportfoliobackend/MySpringPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liur.myspringportfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringPortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
