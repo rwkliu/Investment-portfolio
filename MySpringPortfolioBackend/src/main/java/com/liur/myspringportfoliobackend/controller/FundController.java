5a9f13d18b5121c7d8f1a1727b644ec8dcca4e18
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-05-21 12:08:33 -0700

    Remove Add, Get All, Update, Delete fund controller methods

--- MySpringPortfolioBackend/src/main/java/com/liur/myspringportfoliobackend/controller/FundController.java
+++ MySpringPortfolioBackend/src/main/java/com/liur/myspringportfoliobackend/controller/FundController.java
@@ -2,12 +2,7 @@ package com.liur.myspringportfoliobackend.controller;
 
 import com.liur.myspringportfoliobackend.model.Fund;
 import com.liur.myspringportfoliobackend.service.FundService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @CrossOrigin(originPatterns = "http://localhost:3000")
 @RestController
@@ -19,41 +14,8 @@ public class FundController {
     this.fundService = fundService;
   }
 
-  // Add funds record
-  @PostMapping("/funds")
-  public Fund createFund(@RequestBody Fund fund) {
-    return fundService.createFund(fund);
-  }
-
   @GetMapping("/funds/{fundId}")
   public Fund getFund(@PathVariable Long fundId) {
     return fundService.getFund(fundId);
-  }
-
-  // Get available funds
-  @GetMapping("/funds")
-  public List<Fund> getAllFunds() {
-    return fundService.getAllFunds();
-  }
-
-  // Update fund
-  @RequestMapping(
-    value = "/funds/{fundId}",
-    produces = "application/json",
-    method = {RequestMethod.PUT}
-  )
-  public ResponseEntity<Fund> updateFund(@PathVariable Long fundId, @RequestBody Fund newFundAmount) {
-    newFundAmount = fundService.updateFund(fundId, newFundAmount);
-    return ResponseEntity.ok(newFundAmount);
-  }
-
-  // Delete fund
-  @DeleteMapping("/funds/{fundId}")
-  public ResponseEntity<Map<String, Boolean>> deleteFund(@PathVariable Long fundId) {
-    boolean deleted = false;
-    deleted = fundService.deleteFund(fundId);
-    Map<String, Boolean> response = new HashMap<>();
-    response.put("deleted", deleted);
-    return ResponseEntity.ok(response);
   }
 }
