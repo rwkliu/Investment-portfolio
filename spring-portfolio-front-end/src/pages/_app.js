5f36ac3214a43c64e3fd68c54082a90e711bcb39
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-08-06 20:43:07 -0700

    Import bootstrap Javascript for offline use

--- spring-portfolio-front-end/src/pages/_app.js
+++ spring-portfolio-front-end/src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />
 }
