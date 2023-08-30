a622d4e9f730778d44cd2c5ecade08e1432803f1
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-08-29 18:28:34 -0700

    Fetch investments from database and filter selected investments

--- spring-portfolio-front-end/src/pages/view-selected-investments/[selectedIds].js
+++ spring-portfolio-front-end/src/pages/view-selected-investments/[selectedIds].js
@@ -1,7 +1,24 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { getInvestmentData } from "../../../lib/investmentData";
 
-export default function SelectedInvestments() {
+export async function getServerSideProps() {
+  const investments = await getInvestmentData();
+
+  if (!investments) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      investments
+    }
+  }
+}
+
+export default function SelectedInvestments({ investments }) {
   const router = useRouter();
   const { selectedIds } = router.query;
   let selected = [];
@@ -9,8 +26,9 @@ export default function SelectedInvestments() {
   if (selectedIds) {
     selected = selectedIds.split(","); 
   } 
-  console.log(selected);
 
+  const selectedInvestments = investments.filter((investment) => selectedIds.includes(investment.investmentId));
+  console.log(selectedInvestments);
 
   return (
     <>
